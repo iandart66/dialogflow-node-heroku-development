9ee8ebd161655f6d1f3bb991f6f69c46a9e6a65a
Ian Dart <ian.dart@idc.technology>
2019-11-28 23:30:31 +0000

    28 N0v 19 changes

--- index.js
+++ index.js
@@ -1,9 +1,53 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const { WebhookClient } = require("dialogflow-fulfillment");
+
+require("dotenv").config();
+
+const {
+  dialogflow,
+  actionsdk,
+  Image,
+  Table,
+  Carousel
+} = require("actions-on-google");
+
 const app = express();
 
-app.get("/", function(req, res) {
-  res.send("Hello WWWWorld");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function gedate(agent) {
+  agent.add(
+    "The General Election will take place on Thursday 12th Decemeber 2020"
+  );
+}
+
+function WebhookProcessing(req, res) {
+  const agent = new WebhookClient({ request: req, response: res });
+
+  let intentMap = new Map();
+  intentMap.set("When is the General Election", gedate);
+  agent.handleRequest(intentMap);
+}
+
+app.post("/", (req, res) => {
+  console.log("DialogFlow Server Reached!!!");
+
+  WebhookProcessing(req, res);
 });
 
-const port = process.env.PORT || 5000;
-app.listen(port);
+//app.intent("Default Welcome Intent", conv => {
+//  conv.ask("How are you?");
+//});
+
+//app.intent("when is the general election", conv => {
+//  conv.ask(
+//    "The General Election is to take the place on Thursday 12th December 2020"
+//  );
+//});
+
+const port = process.env.port || 5000;
+app.listen(port, () => {
+  console.log(`App listening on port ${port}!`);
+});
