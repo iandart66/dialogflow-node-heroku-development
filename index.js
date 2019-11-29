51abcaea293a2a20c19cd7e652f4fd53720c3827
iandart66 <58311972+iandart66@users.noreply.github.com>
2019-11-29 13:35:25 +0000

    Update index.js

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.post("/", (req, res) => {
 //  );
 //});
 
-const port = process.env.port || 5000;
-app.listen(port, () => {
-  console.log(`App listening on port ${port}!`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
 });
